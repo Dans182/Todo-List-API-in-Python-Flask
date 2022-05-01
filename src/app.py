50e04d709862f6ba6e889d72e813b41de367c3bf
Dans182 <dgaiteiro@gmail.com>
2022-05-01 11:03:19 +0000

    hasta el 7.1

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import jsonify
+from flask import request
 app = Flask(__name__)
 
 todos = [
@@ -12,6 +13,11 @@ def hello_world():
   json_text = jsonify(todos)
   return json_text
 
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data.json
+    print("Incoming request with the following body", request_body)
+    return 'Response for the POST todo'
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
