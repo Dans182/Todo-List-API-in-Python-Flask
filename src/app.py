a56715afd846e16a8506de4d7abbb2f8ba4b66ce
Dans182 <dgaiteiro@gmail.com>
2022-05-01 12:10:08 +0000

    hasta el 8.2

--- src/app.py
+++ src/app.py
@@ -26,7 +26,9 @@ def add_new_todo():
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
     print("This is the position to delete: ", position)
-    return 'something'
+    todos.pop(position)
+    json_text = jsonify(todos)
+    return json_text
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
